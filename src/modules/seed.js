21df2dfec35e1da8ed54c04826c68a46544475c5
smcommits <sm.anas@outlook.com>
2021-03-17 20:58:26 +0530

    Add title for page.

--- src/modules/seed.js
+++ src/modules/seed.js
@@ -10,7 +10,7 @@ const seedExamples = () => {
     defaultTask.description = `A task manager app where you can create various projects and add tasks assosiated with that project. To create your own project,
                               click the Create New Project button on the left. To add tasks for that project, click Create New Task Button on the top. To edit or delete the task,
                               Please use the Pen icon or Delete icon. The background of task reflects the priority of task. Blue background means the task is not urgent, 
-                              Yellow means the task is important, and the red background means that the task is urgent`
+                              Yellow means the task is important, and the red background means that the task is urgent`;
 
     defaultProject.addTask(defaultTask);
     LocalStorage.refresh(defaultProject);
